ed9ea2bea26c8783ed4b0c92281e306bd27bb0ec
irenehuonline <91905315+irenehuonline@users.noreply.github.com>
2023-03-11 21:27:32 -0800

    bar chart changes

--- main.js
+++ main.js
@@ -2,10 +2,33 @@
 // info-474
 // this is the main js file for the precipitation viz.
 
-// ⭐⭐⭐ IMPORTING RADIAL SCALE
+function dataPreprocessor(row) {
+    const months = {
+        1:  'JAN', 2:  'FEB',
+        3:  'MAR', 4:  'APR',
+        5:  'MAY', 6:  'JUN',
+        7:  'JUL', 8:  'AUG',
+        9:  'SEP', 10: 'OCT',
+        11: 'NOV', 12: 'DEC'
+    }
 
-function onDefault() {
-    console.log('filler code');
+    return {
+        date: row.date,
+        month: months[row.date.split("-")[1]],
+        year: row.date.split("-")[0],
+        // actual_mean_temp
+        // actual_min_temp
+        // actual_max_temp
+        // average_min_temp
+        // average_max_temp
+        // record_min_temp
+        // record_max_temp
+        // record_min_temp_year
+        // record_max_temp_year
+        actual_precipitation: row.actual_precipitation,
+        average_precipitation: row.average_precipitation,
+        record_precipitation: row.record_precipitation
+    }
 }
 
 // 📝 global variable selected year
@@ -24,50 +47,59 @@ var outerRadius = Math.min(svgWidth, svgHeight) / 2;
 
 // 📝 please remember to append any graph elements to chartG!
 //      chartG appended to svg above.
-var chartG = svg.append('g') .attr('transform', 'translate(' + [svgWidth/ 2, svgHeight / 2] + ')')
+var chartG = svg.append('g').attr('transform', 'translate(' + [svgWidth/ 2, svgHeight / 2] + ')')
 
 
 
 // 🌟 write javascript below
-d3.csv('Weather Data/KSEA.csv').then(function(dataset){
+d3.csv('Weather Data/KSEA.csv', dataPreprocessor).then(function(dataset){
     
     // ✅ filtering the dataset - if selectedYear == the date in the dataset
-    weatherPoints = dataset.filter(function(d) {
-        // TO PRINT ALL YEAR DATA POINTS: console.log(d.date.split("-")[0]);
-        return d.date.split("-")[0] == selectedYear;
-    });
+    weatherPoints = dataset
+    // .filter(function(d) {
+    //     // TO PRINT ALL YEAR DATA POINTS: console.log(d.date.split("-")[0]);
+    //     return d.date.split("-")[0] == selectedYear;
+    // });
 
     console.log(weatherPoints);
 
     // ✅ find the max precipitation
     var maxPrecip = d3.max(weatherPoints, function(d){
-        // TO PRINT ALL PRECIPITATION DATA POINTS: console.log(d.actual_precipitation);
-        return d.actual_precipitation;
+        // TO PRINT ALL PRECIPITATION DATA POINTS: console.log(d.average_precipitation);
+        return d.average_precipitation;
     })
 
     console.log("maxPrecip: " + maxPrecip);
-    console.log("")
-    // xScale
+
+    // ✅ xScale
     xScale = d3.scaleBand()
                 .domain(weatherPoints.map(function(d){
-                    console.log(d.date.split("-")[1]);
                     return d.date;
                 }))
                 .range([0, 2 * Math.PI]);
-    // yScale
-    var yScale = scaleRadial()
+    // ✅ yScale
+    var yScale = d3.scaleRadial()
                 .domain([0, maxPrecip])
                 .range([innerRadius, outerRadius]);
-
-
+    // ✅ gradientScale 
+    var gradientScale = d3.scaleLinear()
+                        .domain([0, maxPrecip])
+                        .range([1, 0.25])
+                        
+    
+   
+    // CREATING THE GRAPH FR
     chartG.selectAll("path")
     .data(weatherPoints)
     .enter()
     .append("path")
-      .attr("fill", "black")
-      .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+      .attr("fill", "blue")
+      .attr('fill-opacity', function(d){
+        return gradientScale(d.average_precipitation)
+      })
+      .attr("d", d3.arc()
           .innerRadius(innerRadius)
-          .outerRadius(function(d) { return yScale(d.actual_precipitation); })
+          .outerRadius(function(d) { return yScale(d.average_precipitation); })
           .startAngle(function(d) { return xScale(d.date); })
           .endAngle(function(d) { return xScale(d.date) + xScale.bandwidth(); })
           .padAngle(0.01)
@@ -78,14 +110,21 @@ d3.csv('Weather Data/KSEA.csv').then(function(dataset){
           .data(weatherPoints)
           .enter()
           .append("g")
-            .attr("text-anchor", function(d) { return (xScale(d.date) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
-            .attr("transform", function(d) { return "rotate(" + ((xScale(d.date) + xScale.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (yScale(d.actual_precipitation)+ 0.05) + ",0)"; })
+            .attr("text-anchor", function(d) { 
+                return (xScale(d.date) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; 
+            })
+            .attr("transform", function(d) { 
+                return "rotate(" + ((xScale(d.date) + xScale.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (yScale(d.average_precipitation)+ 5) + ",0)"; 
+            })
             .append("text")
-            .text(function(d){return(d.date.split("-")[1])})
-            .attr("transform", function(d) { return (xScale(d.date) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
-            .style("font-size", "11px")
+            .text(function(d){
+                return(d.month)
+            })
+            .attr("transform", function(d) { 
+                return (xScale(d.date) + xScale.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; 
+            })
+            .style("font-size", "3px")
             .attr("alignment-baseline", "middle")
-    
     });
 
 
