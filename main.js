d5018bbcebe6f555ae0a3bbb10f9fe9c8b91b586
irenehuonline <91905315+irenehuonline@users.noreply.github.com>
2023-03-11 17:01:18 -0800

    progress 1

--- main.js
+++ main.js
@@ -1,23 +1,47 @@
 // irene hu
+// info-474
 // this is the main js file for the precipitation viz.
 
 function onDefault() {
     console.log('filler code');
 }
 
-// 📝 svg
+// 📝 global variable selected year
+selectedYear = 2015;
+
+// 📝 d3 element svg
 var svg = d3.select('svg');
+
 
 // 📝 please remember to append any graph elements to chartG!
 //      chartG appended to svg above.
 var chartG = svg.append('g')
 
 
-d3.csv('Weather Data/KSEA.csv').then(function (dataset){
-    // ⭐ javascript code here!
+// 📝 gets svg element layout parameters from the HTML
+var svgWidth = +svg.attr('width');
+var svgHeight = +svg.attr('height');
+
+
+// 🌟 write javascript below
+d3.csv('Weather Data/KSEA.csv').then(function(dataset){
     
+    // ✅ filtering the dataset - if selectedYear == the date in the dataset
+    weatherPoints = dataset.filter(function(d) {
+        // TO PRINT ALL YEAR DATA POINTS: console.log(d.date.substring(0,4));
+        return d.date.substring(0,4) == selectedYear;
+    });
 
+    // ✅ find the max precipitation
+    var maxPrecip = d3.max(weatherPoints, function(d){
+        // TO PRINT ALL PRECIPITATION DATA POINTS: console.log(d.actual_precipitation);
+        return d.actual_precipitation;
+    })
 
+    console.log("maxPrecip: " + maxPrecip);
+
+    
+    
     // 📝 TO-DO LIST
     // find a good size for the doughnut chart?
     // set 12 different even sections - label by month
